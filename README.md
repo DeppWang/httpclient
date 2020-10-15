e41040328b4fef2a408c7986a68f413aa4341fd0
DeppWang <deppwxq@gmail.com>
2020-10-15 11:37:57 +0800

    Update code

--- README.md
+++ README.md
@@ -1,10 +1,59 @@
-用于像 Postman 一样测试接口，无需像传统测试时，启动项目来调用 Service
+用于像 Postman 一样测试接口，无需像传统测试时，启动 Spirng 来调用 Service
 
 ## 使用 
 
 ### Get
 
+```Java
+	private final String URL = "https://depp.wang";
+
+	@Test
+    void testGet() throws Exception {
+        String result = HttpClientHelper.get(URL);
+        logger.info("[result: {}]", result);
+    }
+```
+
 ### Post
 
+- application/x-www-form-urlencoded
+
+```Java
+    @Test
+    @DisplayName("post application/x-www-form-urlencoded")
+    public void testPostForm() throws Exception {
+        Map<String, Object> form = new HashMap<>();
+        form.put("userId", USER_ID);
+        form.put("address", "3/151 Hartley Road, Smeaton Grange, New South Wales, Australia");
+
+        String url = URL + "/profile/v1/address/update";
+        String result = HttpClientHelper.postForm(url, form);
+        logger.info("[result: {}]", result);
+    }
+```
+
+- application/json
+
+```Java
+    @Test
+    @DisplayName("post application/json")
+    public void testPostJson() throws Exception {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.set("pid", "31502");
+
+        String url = URL + "/profile/v1/address/update";
+        String result = HttpClientHelper.postJSON(url, jsonObject.toString());
+        logger.info("[result: {}]", result);
+    }
+```
+
 ## 原理
+
+本质上还是使用 [HttpClient](https://hc.apache.org/httpcomponents-client-4.5.x/httpclient/apidocs/org/apache/http/client/HttpClient.html) 来实现的
+
+## 打 jar 包
+
+```
+mvn clean package
+```
 
--- src/test/java/wang/depp/httpclient/HttpclientApplicationTests.java
+++ src/test/java/wang/depp/httpclient/HttpclientApplicationTests.java
@@ -1,5 +1,7 @@
 package wang.depp.httpclient;
 
+import cn.hutool.json.JSONObject;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,33 +18,36 @@ class HttpclientApplicationTests {
 
     @Test
     void testGet() throws Exception {
-
         String result = HttpClientHelper.get(URL);
         logger.info("[result: {}]", result);
     }
 
     @Test
     void testGetAddHeader() throws Exception {
-
         String result = HttpClientHelper.get(URL, addHeader());
         logger.info("[result: {}]", result);
     }
 
     @Test
-    public void testPost() throws Exception {
+    @DisplayName("post application/x-www-form-urlencoded")
+    public void testPostForm() throws Exception {
         Map<String, Object> form = new HashMap<>();
         form.put("userId", USER_ID);
         form.put("address", "3/151 Hartley Road, Smeaton Grange, New South Wales, Australia");
-        form.put("unitNumber", "3");
-        form.put("streetNumber", "151");
-        form.put("streetName", "Hartley");
-        form.put("streetType", "Rd");
-        form.put("suburb", "Smeaton Grange");
-        form.put("state", "NSW");
-        form.put("postcode", "2567");
 
         String url = URL + "/profile/v1/address/update";
-        String result = HttpClientHelper.postForm(url, form, addHeader());
+        String result = HttpClientHelper.postForm(url, form);
+        logger.info("[result: {}]", result);
+    }
+
+    @Test
+    @DisplayName("post application/json")
+    public void testPostJson() throws Exception {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.set("pid", "31502");
+
+        String url = URL + "/profile/v1/address/update";
+        String result = HttpClientHelper.postJSON(url, jsonObject.toString());
         logger.info("[result: {}]", result);
     }
 
